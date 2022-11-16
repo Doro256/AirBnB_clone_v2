c2e1a8781e43766a04b3c1bfbf02b18455a133b0
Doro256 <dorothynantumbwe@yahoo.com>
2022-11-16 15:08:28 +0000

    make executable

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Simple Flask web application"""
 from flask import Flask
-app = Flask('web_flask')
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
@@ -11,4 +11,4 @@ def hello_route():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
